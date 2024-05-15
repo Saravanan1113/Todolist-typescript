fbbe892b064e169411de0028abd1f9eb093ce13f
Saravanan1113 <saravanarajendran1998@gmail.com>
2024-05-15 11:43:27 +0530

    first commit

---
+++ src/components/todoTask.tsx
@@ -0,0 +1,25 @@
+import { ITask } from "../interface";
+
+interface Props {
+  task: ITask;
+  completeTask(taskToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadLine}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+export default TodoTask;
